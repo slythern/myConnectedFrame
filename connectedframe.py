27b726fa692dac760ec75912f0cb0f4cd937b2df
slythern <31569641+slythern@users.noreply.github.com>
2017-09-20 15:58:51 -0500

    Update connectedframe.py
    added ignore directories and limit download to images to unzip command

--- connectedframe.py
+++ connectedframe.py
@@ -22,7 +22,7 @@ def download_images(url):
 
 	remove = "sudo rm -rf " + base_path + "*"
 	download = "wget -q  "+ url + " -O " + archive
-	extract = "unzip -o " + archive + " -d " + base_path
+	extract = "unzip -o -j " + archive + " *.jpg -d " + base_path # added *.jpg to get only images, added -j to not make directories
 
 	system(remove)
 	print("download")
@@ -33,6 +33,7 @@ def download_images(url):
 def resize_images():
 	images = list_images()
 
+	print("resize")
 	for file in images:
 		img = Image.open(file)
 		img = img.resize((640, 480), Image.ANTIALIAS)
@@ -115,7 +116,7 @@ def initialize():
 
 	if(initial_init):
 		initial_init = False
-#		root.after(1000, initialize)
+#		root.after(1000, initialize) # removed to stop loading images twice
 	else:
 		root.after(download_interval, initialize)
 
